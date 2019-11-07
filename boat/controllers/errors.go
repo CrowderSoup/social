ed6bf09cf382ae32088ddeb2e7b3d9541afb63d6
Aaron Crowder <aaron.crowder@gmail.com>
2019-11-07 15:34:36 -0700

    Refactor error handling

---
+++ boat/controllers/errors.go
@@ -0,0 +1,30 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+
+	echo "github.com/labstack/echo/v4"
+)
+
+// HTTPErrorHandler handle errors
+func HTTPErrorHandler(err error, ctx echo.Context) {
+	code := http.StatusInternalServerError
+	message := "Something went wrong"
+	if he, ok := err.(*echo.HTTPError); ok {
+		code = he.Code
+		message = fmt.Sprint(he.Message)
+	}
+
+	page := "5xx"
+	if code < http.StatusInternalServerError {
+		page = "4xx"
+	}
+
+	fmt.Println(err)
+	ctx.Render(http.StatusUnauthorized, page, echo.Map{
+		"title": "SocialMast - Uh Oh!",
+		"error": message,
+		"code":  code,
+	})
+}
